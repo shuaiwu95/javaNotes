9b8d94e1ba8c5aacfca13c54c0ba87df1f7eb8f1
shuaiwuLi <shuaiwu123@foxmail.com>
2021-01-16 18:33:17 +0800

    测试idea

--- TwoDimArray.java
+++ TwoDimArray.java
@@ -13,7 +13,7 @@ public class TwoDimArray {
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + "\t");
+                System.out.print(arr[i][j] + "\t 222");
             }
             System.out.println(Arrays.toString(arr[i]));
         }
